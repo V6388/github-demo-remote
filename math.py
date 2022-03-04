9a845955ba50a94f196f871852924194525f01cd
vipinyadav <vipinyadav.delhi6388@gmail.com>
2022-03-04 19:05:51 +0530

    Added fn headrs

--- math.py
+++ math.py
@@ -1,12 +1,16 @@
 #add implementation.
+#jst added
 def add(x,y):
     pass
-    
+
+# subtract implementation
 def subtract(x,y):
     pass
     
+# multiply implementation
 def multiply(x,y):
     pass
     
+# divideimplementation
 def divide(x,y):
     pass
